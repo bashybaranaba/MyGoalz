6e91f9aa978c48419b36f7bba34cccee7b68cf02
Abykirwa1937 <Abigaelkirwa1937@gmail.com>
2022-11-18 17:50:39 +0300

    login, signup, splash screen, and edit profile

--- app/src/main/java/com/example/mygoalz/fragments/SplashFragment.kt
+++ app/src/main/java/com/example/mygoalz/fragments/SplashFragment.kt
@@ -1,6 +1,9 @@
 package com.example.mygoalz.fragments
 
+import android.content.Intent
 import android.os.Bundle
+
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,20 +13,21 @@ import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.mygoalz.R
 import com.example.mygoalz.databinding.FragmentSignUpBinding
+import android.os.Handler
+import com.example.mygoalz.databinding.FragmentSplashBinding
 
-
-class SignUpFragment : Fragment() {
-
+class SplashFragment : Fragment() {
     private lateinit var navControl: NavController
-    private lateinit var binding: FragmentSignUpBinding
+    private lateinit var binding: FragmentSplashBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentSignUpBinding.inflate(inflater, container, false)
+        binding = FragmentSplashBinding.inflate(inflater, container, false)
         return binding.root
+        
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -33,16 +37,9 @@ class SignUpFragment : Fragment() {
     }
 
 
-    private fun init(view:View) {
+    private fun init(view: View) {
         navControl = Navigation.findNavController(view)
-        binding.signUp.setOnClickListener{
-            Toast.makeText(context, "Registered successfully", Toast.LENGTH_SHORT).show()
-            navControl.navigate(R.id.action_signUpFragment_to_logInFragment)
-        }
-
-        binding.textSignIn.setOnClickListener{
-            navControl.navigate(R.id.action_signUpFragment_to_logInFragment)
-        }
+        navControl.navigate(R.id.action_splashFragment_to_signUpFragment3)
 
     }
 }
