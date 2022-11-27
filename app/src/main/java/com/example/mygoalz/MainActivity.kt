548e44561994a912d2b45e6e2843cafa210272fb
DanYT2 <danwwititu@gmail.com>
2022-11-27 19:53:36 +0300

    Removed replaceFragment(HomeFragment())

--- app/src/main/java/com/example/mygoalz/MainActivity.kt
+++ app/src/main/java/com/example/mygoalz/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        replaceFragment(HomeFragment())
+
 
         binding.bottomNavigationView.setOnItemSelectedListener {
             when(it.itemId){
